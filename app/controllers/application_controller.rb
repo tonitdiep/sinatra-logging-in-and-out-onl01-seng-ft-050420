a2befd367d2fb786ec943071aca23750ce918634
Toni  Diep <adiep123@gmail.com>
2020-07-05 22:11:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
-      if @user && @user.authenticate(password: params[:password]) ==@user.password
+    # @user = User.find_by(username: params[:username])
+    #   if @user && @user.authenticate(password: params[:password]) ==@user.password
       # if @user != nil && @user.password == params[:password]
+      @user = User.find_by(username: params[:username], password: params[:password])
+        if @user
         session[:user_id] = @user.id
         redirect to '/account'
       else
